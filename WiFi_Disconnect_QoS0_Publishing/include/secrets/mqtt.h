aab1bc0abe118e887f2308817f591e58b6ea503c
Nguyễn Anh Tuấn <tuanphysics@hus.edu.vn>
2024-10-24 11:47:16 +0700

    Delete WiFi_Disconnect_QoS0_Publishing/include/secrets directory

--- WiFi_Disconnect_QoS0_Publishing/include/secrets/mqtt.h
+++
@@ -1,8 +0,0 @@
-namespace MQTT
-{
-    const char *broker = "ce06ae2c.ala.eu-central-1.emqxsl.com";
-    const int port = 8883;
-
-    const char *username = "tuan";
-    const char *password = "tTCekZ3eQCh7";
-}
