bcb3237e1ba62ec58496945c8160556627679bd1
Tran Tuan Phong <trantuanphong09092003@gmail.com>
2024-10-31 01:19:32 +0700

    hide secrets

--- WiFi_Disconnect_QoS0_Publishing/src/secrets/mqtt.h
+++
@@ -1,7 +0,0 @@
-namespace MQTT
-{
-    const char *username = "TuanPhong";
-    const char *password = "Pp@0869037660";
-    const char *broker = "48a113f51f254502a6fc327abf6fe6a5.s1.eu.hivemq.cloud";
-    uint16_t port = 8883;
-}
--- WiFi_Disconnect_QoS0_Publishing/src/secrets/wifi.h
+++
@@ -1,9 +0,0 @@
-namespace WiFiSecrets
-{
-    const char *ssid = "P404";
-    const char *pass = "66668888404";
-    const char *echo_topic = "esp32/echo_test";
-    unsigned int publish_count = 0;
-    uint16_t keepAlive = 15;
-    uint16_t socketTimeout = 5;
-}
